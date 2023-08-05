e719acab7bf8e2e46313d5c0c4e7a7ca6d2ffda3
lmcdeeshann <134543505+lmcdeeshann@users.noreply.github.com>
2023-08-05 16:12:11 +0530

    second commit. pages created
    pages created and routes added

--- socialApp1/src/index.tsx
+++ socialApp1/src/index.tsx
@@ -5,14 +5,34 @@ import { Header } from './components/Header.jsx';
 import { Home } from './pages/Home/index.jsx';
 import { NotFound } from './pages/_404.jsx';
 import './style.css';
+import { login } from './pages/Auth/login.js';
+import { registration } from './pages/Auth/registration.js';
+import { forgetpassword } from './pages/Auth/forgetpassword.js';
+import { profile } from './pages/Profile/profile.js';
+import { useEffect } from 'preact/hooks';
+import { route } from 'preact-router';
 
 export function App() {
+	function ProtectedRoute(props) {
+		//const { isAuthenticated } = useAuthContext();
+	
+		//useEffect(() => {
+		//	if (!isAuthenticated) route('/login', true);
+		//}, [isAuthenticated]);
+	
+		return <Route {...props} />;
+	}
+	
 	return (
 		<LocationProvider>
 			<Header />
 			<main>
 				<Router>
-					<Route path="/" component={Home} />
+					<ProtectedRoute path="/" component={Home} />
+					<Route path='/login' component={login} />
+					<Route path='/registration' component={registration} />
+					<Route path='/forgetpassword' component={forgetpassword} />
+					<ProtectedRoute path='/profile' component={profile} />
 					<Route default component={NotFound} />
 				</Router>
 			</main>
@@ -21,3 +41,7 @@ export function App() {
 }
 
 render(<App />, document.getElementById('app'));
+function useAuthContext(): { isAuthenticated: any; } {
+	throw new Error('Function not implemented.');
+}
+
