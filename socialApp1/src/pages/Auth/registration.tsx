1b7e084339e989d0df2d289f85f7058198207fff
Jeewantha De Silva <100590888+IT21228612@users.noreply.github.com>
2023-08-20 15:57:07 +0530

    update registration

--- socialApp1/src/pages/Auth/registration.tsx
+++ socialApp1/src/pages/Auth/registration.tsx
@@ -2,6 +2,7 @@ import preactLogo from '../../assets/preact.svg';
 
 export function registration() {
 	return (
+<<<<<<< Updated upstream
 		<div class="home">
 			<a href="https://preactjs.com" target="_blank">
 				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
@@ -25,6 +26,9 @@ export function registration() {
 				/>
 			</section>
 		</div>
+=======
+		<div>jj</div>
+>>>>>>> Stashed changes
 	);
 }
 
