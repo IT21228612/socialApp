8d586d1d245e0e55e2471850b867cba717edd594
Jeewantha De Silva <100590888+IT21228612@users.noreply.github.com>
2023-08-25 19:46:48 +0530

    login page done
    login page UI is done

--- socialApp1/src/pages/Auth/login.tsx
+++ socialApp1/src/pages/Auth/login.tsx
@@ -1,60 +1,52 @@
 import React from 'react';
 
 import './login.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export function login() {
 
 	return(
-
 		
 
-		<div className="page">
- 
-		<div className="row">
-		  
-		
-		  <div className="col-sm-8 text-center signin">
-		
-		
-		   
-			<form action="">
-	  
-			  <p className="h4 mb-4 text-center signintext top"><strong>Sign in to this Website</strong></p>
-		
-		
-		<div className="social-btn text-center">
-			  <a href="#" className="btn btn-primary btn-lg" title="Facebook"><i className="fa fa-facebook"></i></a>
-			  <a href="#" className="btn btn-info btn-lg" title="Twitter"><i className="fa fa-twitter"></i></a>
-			  <a href="#" className="btn btn-danger btn-lg" title="Pinterest"><i className="fa fa-pinterest"></i></a>
-			</div>
-		
-			  <p className="text-center signintext">Or use your Email account</p>
-		
-		 
-			  <input type="email" className="form-control mb-4"  placeholder="&#xf0e0; Email" style="font-family:Arial, FontAwesome" />
-		
-			
-			  <input type="password" className="form-control mb-4" placeholder="&#xf023;  Password" style="font-family:Arial, FontAwesome" />
-		
-			 
-		
-					<a className="ForgotPassBtn" href="" >Forgot Your Password?</a>
-			  <button className="btn btn-info btn-block LoginBtn" type="submit" >SIGN IN</button>
-		
-		
-			</form>
-		
-		  </div>
-		
-		
-		  <div className="col-sm-4 signup text-center">
-			<h2 className="HelloFriend">Hello, Friend!</h2>
-			<h4 className="SignupText">Enter your personal details<br/>and start journey with us</h4>
-			<button className="btn btn-info btn-block signupbtn" type="submit">SIGN UP</button>
-		
-		  </div>
-		</div>
-		</div>
+<div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card-group mb-0">
+          <div class="card p-4">
+            <div class="card-body">
+              <h1>Login</h1>
+              <p class="text-muted">Sign In to your account</p>
+              <div class="input-group mb-3">
+                <span class="input-group-addon"><i class="fa fa-user" id="user-icon"></i></span>
+                <input type="text" class="form-control" placeholder="Username"/>
+              </div>
+              <div class="input-group mb-4">
+                <span class="input-group-addon"><i class="fa fa-lock" id="lock-icon"></i></span>
+                <input type="password" class="form-control" placeholder="Password"/>
+              </div>
+              <div class="row">
+                <div class="col-6">
+                  <button type="button" class="btn btn-primary px-4">Login</button>
+                </div>
+                <div class="col-6 text-right">
+                  <button type="button" class="btn btn-link px-0">Forgot password?</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card text-white  py-5 d-md-down-none" id="card1" style="width:44%">
+            <div class="card-body text-center">
+              <div>
+                <h2>Sign up</h2>
+                <p>Create memories, make friends, and explore together.</p>
+                <button type="button" class="btn btn-primary active mt-3">Register Now!</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 		  );
 		}
 		
